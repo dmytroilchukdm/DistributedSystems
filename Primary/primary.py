9a7d3ebb086d437db587825f20a93b19744a538d
dmytroilchukdm <ilchuk0704@gmail.com>
2024-10-14 00:32:34 +0300

    First attempt of the Replicated log assignment

--- Primary/primary.py
+++ Primary/primary.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 import json
+import logging
+logging.basicConfig(level=logging.INFO)
 
 
 class PrimaryHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -10,8 +12,7 @@ class PrimaryHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type","application/json")
         self.end_headers()
         json_data = json.dumps(self.messageList)
-        print("list to send from server", self.messageList)
-        print("list to send from server",json_data)
+        logging.info(f"list to send from server {self.messageList}")
         self.wfile.write(bytes(json_data, "utf-8"))
         self.wfile.write(bytes("\n", "utf-8"))
 #        self.wfile.write(repr(self.messageList).encode())
@@ -23,19 +24,19 @@ class PrimaryHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.messageList.append(post_data.decode("utf-8"))
-        print("The message list",self.messageList)
+        replicated_data = post_data.decode("utf-8")
+        logging.info(f"Received data to forward {replicated_data}")
+        self.messageList.append(replicated_data)
+        logging.info(f"The message list on primary {self.messageList}")
         requests.post('http://secondary1:9001', data=post_data)
         requests.post('http://secondary2:9002', data=post_data)
-        response = f"Data received: {post_data.decode()}"
-        print(response)
         #self.wfile.write(response.encode())
         #self.wfile.write(repr(self.messageList).encode())
 
 def run(server_class=HTTPServer, handler_class=PrimaryHTTPRequestHandler, port=9000):
     server_address = ('', port)
     primary = server_class(server_address, handler_class)
-    print(f"Initiating primary on port {port}")
+    logging.info(f"Initiating primary on port {port}")
     primary.serve_forever()
 
 
--- Secondary/secondary.py
+++ Secondary/secondary.py
@@ -2,6 +2,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import sys
 import time
+import logging
+logging.basicConfig(level=logging.INFO)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     messageList = []
@@ -10,8 +12,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type","application/json")
         self.end_headers()
         json_data = json.dumps(self.messageList)
-        print("list to send from secondary", self.messageList)
-        print("list to send from secondary",json_data)
+        logging.info(f"list to send from secondary {self.messageList}")
         self.wfile.write(bytes(json_data, "utf-8"))
         self.wfile.write(bytes("\n", "utf-8"))
 
@@ -23,21 +24,20 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.messageList.append(post_data.decode("utf-8"))
-        print("The message list",self.messageList)
-        response = f"Data received: {post_data.decode()}"
-        print(response)
+        data_received = post_data.decode("utf-8")
+        logging.info(f"Data received: {data_received}")
+        self.messageList.append(data_received)
+        logging.info(f"The message list {self.messageList}")
         #self.wfile.write(response.encode())
         #self.wfile.write(repr(self.messageList).encode())
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=9001):
     server_address = ('', port)
     secondary = server_class(server_address, handler_class)
-    print(f"Initiating secondary on port {port}")
+    logging.info(f"Initiating secondary on port {port}")
     secondary.serve_forever()
 
 if __name__ == "__main__":
-    import sys
     run(port=int(sys.argv[1]))
 
 
